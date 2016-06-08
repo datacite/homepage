68386babc02416a1d66a109a192b60bf64fac4a6
Laura Rueda <Laura@Lauras-MacBook-Pro-2.local>
2016-06-08 19:26:01 +0200

    Version 0. Missing: tech doc, outreach, members list, how to join, fix terms and conditions.

--- Gemfile
+++ Gemfile
@@ -10,5 +10,3 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
 # Middleman Gems
 gem 'middleman', '>= 4.0.0'
-gem 'middleman-dotenv', '~> 2.0'
-gem 'middleman-livereload'
--- config.rb
+++ config.rb
@@ -9,22 +9,14 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
 # General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
-# Set markdown template engine
-set :markdown_engine, :kramdown
-set :markdown, :fenced_code_blocks => true, :smartypants => true
-
-# put configuration variables into .env file
-activate :dotenv
-
-# use asset host
-activate :asset_host, host: "//#{ENV['CDN_HOST']}"
 
 ###
 # Helpers
@@ -36,15 +28,6 @@ activate :asset_host, host: "//#{ENV['CDN_HOST']}"
 #     "Helping"
 #   end
 # end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  activate :minify_css
-
-  # Minify Javascript on build
-  activate :minify_javascript
-end
 
 # Build-specific configuration
 configure :build do
