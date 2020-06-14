613ffe2112778e9db16a35f28565369bf407e021
Martin Fenner <martin.fenner@datacite.org>
2020-06-14 11:25:13 +0200

    remove obsolete file from webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: {
     blog: './source/javascripts/blog.js',
-    country_code: './source/javascripts/country_code.js',
     members: './source/javascripts/members.js',
     homepage: './source/stylesheets/homepage.scss',
     application: './source/stylesheets/application.scss',
@@ -26,38 +25,3 @@ module.exports = {
     path: path.resolve(__dirname, '.tmp/dist')
   }
 };
-
-// module.exports = {
-//   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-//   entry: {
-//     blog: './source/javascripts/blog.js',
-//     country_code: './source/javascripts/country_code.js',
-//     members: './source/javascripts/members.js',
-//     homepage: './source/stylesheets/homepage.scss',
-//     application: './source/stylesheets/application.scss',
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(t|j)sx?$/,
-//         loader: 'babel-loader',
-//         exclude: /node_modules/
-//       },
-//       {
-//         test: /\.(sa|sc|c)ss$/,
-//         use: ['style-loader', 'css-loader', 'sass-loader']
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader']
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: [".js", ".jsx", ".ts", ".tsx"]
-//   },
-//   output: {
-//     path: path.resolve(__dirname, '.tmp/dist'),
-//     filename: 'javascripts/[name].js',
-//   },
-// }
