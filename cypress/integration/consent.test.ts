c1ff156f49e168d85ef8367facfb7bc67c2dae88
Martin Fenner <martin.fenner@datacite.org>
2021-02-02 12:12:11 +0100

    add cypress for automated tests #124 #125

---
+++ cypress/integration/consent.test.ts
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+/* eslint-disable no-undef */
+
+describe("Consent", () => {
+  const sizes = ["iphone-6", "samsung-s10", "ipad-2", [1024, 768]];
+
+  sizes.forEach((size) => {
+    it(`index on ${size} screen`, () => {
+      if (Cypress._.isArray(size)) {
+        cy.viewport(size[0], size[1]);
+      } else {
+        cy.viewport(size);
+      }
+
+      cy.getCookie("_consent").should("not.exist");
+
+      cy.visit("/");
+      cy.get(".CookieConsent", { timeout: 30000 }).contains(
+        "We use cookies on our website. Some are technically necessary, others help us improve your user experience."
+      );
+      cy.get("#rcc-confirm-button").click();
+      cy.getCookie("_consent").should("have.property", "value", "true");
+      cy.get("h1", { timeout: 30000 }).contains("Welcome to DataCite");
+    });
+  });
+});
